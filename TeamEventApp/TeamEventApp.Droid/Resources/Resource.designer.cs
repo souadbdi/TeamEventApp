493ae6b18d918f93a5ddde55f8de8b62422f5f99
hesrondev <hesron@live.fr>
2016-01-19 15:28:40 +0100

    Recadrage des items du Profil

--- TeamEventApp/TeamEventApp.Droid/Resources/Resource.designer.cs
+++ TeamEventApp/TeamEventApp.Droid/Resources/Resource.designer.cs
@@ -224,157 +224,163 @@ namespace TeamEventApp.Droid
 			public const int Cancel = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int com_facebook_button_background = 2130837507;
+			public const int Checked = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int com_facebook_button_icon = 2130837508;
+			public const int com_facebook_button_background = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int com_facebook_button_like_background = 2130837509;
+			public const int com_facebook_button_icon = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int com_facebook_button_like_icon_selected = 2130837510;
+			public const int com_facebook_button_like_background = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int com_facebook_button_login_silver_background = 2130837511;
+			public const int com_facebook_button_like_icon_selected = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int com_facebook_button_send_background = 2130837512;
+			public const int com_facebook_button_login_silver_background = 2130837512;
 			
 			// aapt resource value: 0x7f020009
-			public const int com_facebook_button_send_icon = 2130837513;
+			public const int com_facebook_button_send_background = 2130837513;
 			
 			// aapt resource value: 0x7f02000a
-			public const int com_facebook_close = 2130837514;
+			public const int com_facebook_button_send_icon = 2130837514;
 			
 			// aapt resource value: 0x7f02000b
-			public const int com_facebook_profile_picture_blank_portrait = 2130837515;
+			public const int com_facebook_close = 2130837515;
 			
 			// aapt resource value: 0x7f02000c
-			public const int com_facebook_profile_picture_blank_square = 2130837516;
+			public const int com_facebook_profile_picture_blank_portrait = 2130837516;
 			
 			// aapt resource value: 0x7f02000d
-			public const int com_facebook_tooltip_black_background = 2130837517;
+			public const int com_facebook_profile_picture_blank_square = 2130837517;
 			
 			// aapt resource value: 0x7f02000e
-			public const int com_facebook_tooltip_black_bottomnub = 2130837518;
+			public const int com_facebook_tooltip_black_background = 2130837518;
 			
 			// aapt resource value: 0x7f02000f
-			public const int com_facebook_tooltip_black_topnub = 2130837519;
+			public const int com_facebook_tooltip_black_bottomnub = 2130837519;
 			
 			// aapt resource value: 0x7f020010
-			public const int com_facebook_tooltip_black_xout = 2130837520;
+			public const int com_facebook_tooltip_black_topnub = 2130837520;
 			
 			// aapt resource value: 0x7f020011
-			public const int com_facebook_tooltip_blue_background = 2130837521;
+			public const int com_facebook_tooltip_black_xout = 2130837521;
 			
 			// aapt resource value: 0x7f020012
-			public const int com_facebook_tooltip_blue_bottomnub = 2130837522;
+			public const int com_facebook_tooltip_blue_background = 2130837522;
 			
 			// aapt resource value: 0x7f020013
-			public const int com_facebook_tooltip_blue_topnub = 2130837523;
+			public const int com_facebook_tooltip_blue_bottomnub = 2130837523;
 			
 			// aapt resource value: 0x7f020014
-			public const int com_facebook_tooltip_blue_xout = 2130837524;
+			public const int com_facebook_tooltip_blue_topnub = 2130837524;
 			
 			// aapt resource value: 0x7f020015
-			public const int Comments = 2130837525;
+			public const int com_facebook_tooltip_blue_xout = 2130837525;
 			
 			// aapt resource value: 0x7f020016
-			public const int Comments_Filled = 2130837526;
+			public const int Comments = 2130837526;
 			
 			// aapt resource value: 0x7f020017
-			public const int Details = 2130837527;
+			public const int Comments_Filled = 2130837527;
 			
 			// aapt resource value: 0x7f020018
-			public const int Event = 2130837528;
+			public const int Details = 2130837528;
 			
 			// aapt resource value: 0x7f020019
-			public const int fEdit = 2130837529;
+			public const int Event = 2130837529;
 			
 			// aapt resource value: 0x7f02001a
-			public const int fGroup = 2130837530;
+			public const int fEdit = 2130837530;
 			
 			// aapt resource value: 0x7f02001b
-			public const int fLocation = 2130837531;
+			public const int fGroup = 2130837531;
 			
 			// aapt resource value: 0x7f02001c
-			public const int fOk = 2130837532;
+			public const int fLocation = 2130837532;
 			
 			// aapt resource value: 0x7f02001d
-			public const int fProfile = 2130837533;
+			public const int fOk = 2130837533;
 			
 			// aapt resource value: 0x7f02001e
-			public const int fSettings = 2130837534;
+			public const int fProfile = 2130837534;
 			
 			// aapt resource value: 0x7f02001f
-			public const int Group = 2130837535;
+			public const int fSettings = 2130837535;
 			
 			// aapt resource value: 0x7f020020
-			public const int Home = 2130837536;
+			public const int Group = 2130837536;
 			
 			// aapt resource value: 0x7f020021
-			public const int Icon = 2130837537;
+			public const int Home = 2130837537;
 			
 			// aapt resource value: 0x7f020022
-			public const int Important = 2130837538;
+			public const int Icon = 2130837538;
 			
 			// aapt resource value: 0x7f020023
-			public const int Members = 2130837539;
+			public const int Important = 2130837539;
 			
 			// aapt resource value: 0x7f020024
-			public const int Members_Filled = 2130837540;
+			public const int Members = 2130837540;
 			
 			// aapt resource value: 0x7f020025
-			public const int messenger_bubble_large_blue = 2130837541;
+			public const int Members_Filled = 2130837541;
 			
 			// aapt resource value: 0x7f020026
-			public const int messenger_bubble_large_white = 2130837542;
+			public const int messenger_bubble_large_blue = 2130837542;
 			
 			// aapt resource value: 0x7f020027
-			public const int messenger_bubble_small_blue = 2130837543;
+			public const int messenger_bubble_large_white = 2130837543;
 			
 			// aapt resource value: 0x7f020028
-			public const int messenger_bubble_small_white = 2130837544;
+			public const int messenger_bubble_small_blue = 2130837544;
 			
 			// aapt resource value: 0x7f020029
-			public const int messenger_button_blue_bg_round = 2130837545;
+			public const int messenger_bubble_small_white = 2130837545;
 			
 			// aapt resource value: 0x7f02002a
-			public const int messenger_button_blue_bg_selector = 2130837546;
+			public const int messenger_button_blue_bg_round = 2130837546;
 			
 			// aapt resource value: 0x7f02002b
-			public const int messenger_button_send_round_shadow = 2130837547;
+			public const int messenger_button_blue_bg_selector = 2130837547;
 			
 			// aapt resource value: 0x7f02002c
-			public const int messenger_button_white_bg_round = 2130837548;
+			public const int messenger_button_send_round_shadow = 2130837548;
 			
 			// aapt resource value: 0x7f02002d
-			public const int messenger_button_white_bg_selector = 2130837549;
+			public const int messenger_button_white_bg_round = 2130837549;
 			
 			// aapt resource value: 0x7f02002e
-			public const int Minus = 2130837550;
+			public const int messenger_button_white_bg_selector = 2130837550;
 			
 			// aapt resource value: 0x7f02002f
-			public const int Ok = 2130837551;
+			public const int Minus = 2130837551;
 			
 			// aapt resource value: 0x7f020030
-			public const int Profile = 2130837552;
+			public const int Ok = 2130837552;
 			
 			// aapt resource value: 0x7f020031
-			public const int Profilef = 2130837553;
+			public const int Profile = 2130837553;
 			
 			// aapt resource value: 0x7f020032
-			public const int QuoteLeft = 2130837554;
+			public const int Profilef = 2130837554;
 			
 			// aapt resource value: 0x7f020033
-			public const int QuoteRight = 2130837555;
+			public const int QuoteLeft = 2130837555;
 			
 			// aapt resource value: 0x7f020034
-			public const int Search = 2130837556;
+			public const int QuoteRight = 2130837556;
 			
 			// aapt resource value: 0x7f020035
-			public const int ToDo = 2130837557;
+			public const int Search = 2130837557;
+			
+			// aapt resource value: 0x7f020036
+			public const int ToDo = 2130837558;
+			
+			// aapt resource value: 0x7f020037
+			public const int TrueFalse = 2130837559;
 			
 			static Drawable()
 			{
