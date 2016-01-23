3a00f9111cf282f32751d6cda2d1a039bac1074c
souadbdi <belmehdi.souad78@gmail.com>
2016-01-24 00:40:14 +0100

    Affichage du profil du current user + edition profil et status ok

--- TeamEventApp/TeamEventApp.Droid/Activities/ProfileActivity.cs
+++ TeamEventApp/TeamEventApp.Droid/Activities/ProfileActivity.cs
@@ -10,27 +10,74 @@ namespace TeamEventApp.Droid
     [Activity(Label = "@string/label_profile")]
     public class ProfileActivity : Activity
     {
+        private TextView pseudo;
+        private TextView email;
+        private TextView adr;
+        private TextView events_number;
+        private TextView groups_number;
+        private TextView status;
+        private TextView demandes_groupes;
+        private TextView contacts;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Profile);
 
+            //initialisation du profil
+            pseudo = FindViewById<TextView>(Resource.Id.profile_name);
+            pseudo.Text = MainActivity.database.current_user.pseudo;
+
+            email = FindViewById<TextView>(Resource.Id.profile_email);
+            email.Text = MainActivity.database.current_user.email;
+
+            adr = FindViewById<TextView>(Resource.Id.profile_location);
+            adr.Text = MainActivity.database.current_user.localisation;
+
+            events_number = FindViewById<TextView>(Resource.Id.profile_events_number);
+            //nombre d'évènements du user (parcourir tous ses groupes)
+
+            groups_number = FindViewById<TextView>(Resource.Id.profile_groups_number);
+            groups_number.Text = MainActivity.database.current_user.groups.Count.ToString();
+
+            status = FindViewById<TextView>(Resource.Id.profile_status_text);
+            status.Text = MainActivity.database.current_user.status;
+
+            demandes_groupes = FindViewById<TextView>(Resource.Id.profile_req_number);
+            //demandes d'ajout groupe
+
+            //action edit profil
+            ImageButton edit = FindViewById<ImageButton>(Resource.Id.profile_editProfile_btn);
+            edit.Click += delegate
+            {
+                FragmentTransaction tx = FragmentManager.BeginTransaction();
+                ProfilEditProfileFragment contactsDialog = new ProfilEditProfileFragment();
+                contactsDialog.Show(tx, "Modifier le profil");
+            };
+
+            //action edit status
+            ImageButton edit_status = FindViewById<ImageButton>(Resource.Id.profile_editStatus_btn);
+            edit_status.Click += delegate
+            {
+                FragmentTransaction tx = FragmentManager.BeginTransaction();
+                ProfilEditStatusFragment contactsDialog = new ProfilEditStatusFragment();
+                contactsDialog.Show(tx, "Modifier votre status");
+            };
+
             // Actions pour la liste des demandes à des événements
             TextView textReqList = FindViewById<TextView>(Resource.Id.profile_req_text);
-
             textReqList.Click += delegate
             {
                 StartActivity(typeof(UserGroupRequestActivity));
             };
 
             //Actions pour l'affichage des contacts
-
-            TextView ctextview = FindViewById<TextView>(Resource.Id.contact_tv);
-            ctextview.Click += delegate
+            contacts = FindViewById<TextView>(Resource.Id.profile_contacts_text);
+            contacts.Click += delegate
             {
                 FragmentTransaction tx = FragmentManager.BeginTransaction();
                 ProfilContactsFragment contactsDialog = new ProfilContactsFragment();
-               contactsDialog.Show(tx, "Contacts");
+                contactsDialog.Show(tx, "Contacts");
             };
         }        
     }
---
+++ TeamEventApp/TeamEventApp.Droid/Fragments/ProfilEditStatusFragment.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace TeamEventApp.Droid.Fragments
+{
+    public class ProfilEditStatusFragment : DialogFragment
+    {
+        EditText status;
+        Button edit;
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+
+            var view = inflater.Inflate(Resource.Layout.EditProfileStatus, container, false);
+
+            //instanciation des edit text
+            status = view.FindViewById<EditText>(Resource.Id.edit_status_text);
+
+            //action édition
+            edit = view.FindViewById<Button>(Resource.Id.edit_status_btn);
+            edit.Click += delegate
+            {
+                MainActivity.database.current_user.status = status.Text;
+                Activity.StartActivity(typeof(ProfileActivity));
+            };
+
+            // Return
+            return view;
+        }
+    }
+}
